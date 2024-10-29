8eca920de404b2a8b5815e3e47caffe28ab3bc22
BOPCONTROL <bopcontrol94@gmail.com>
2024-10-29 16:45:06 -0600

    Modificacion del archivo app.js se agrego un text

--- App.js
+++ App.js
@@ -59,6 +59,7 @@ function HomeScreen() {
           },
         ]}/>
       <ScrollView>
+      <Text>BOP CONTROL Belen</Text>
       <Text style={styles.texto1}>Registro de Ganado Bovino</Text>
       <View>
         <Text value={numarete} onChangeText={(numarete) => {setArete(numarete)}} style={{fontSize: 17, fontWeight: '400', color: 'black'}}>Número de arete: </Text>
